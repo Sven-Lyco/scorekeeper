abfe48d6056bc0bcab72d99e6cc945caf5d6f938
Sven Lüchow <sven.luechow@icloud.com>
2022-03-08 18:56:29 +0100

    Change Style of Input and Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Input from './components/Input';
+import Button from './components/Button';
+
 function App() {
-  return <h1>Scorekeeper</h1>;
+  return (
+    <>
+      <h1>Scorekeeper</h1>
+      <Input labelText={'Name of Game'} placeholder="e.g. Carcassonne" />
+      <Input labelText={'Player Names'} placeholder="e.g. John Doe, Jane Doe" />
+      <Button children={'Create Game'} />
+    </>
+  );
 }
 
 export default App;
