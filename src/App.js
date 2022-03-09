d98b1e4b0ce85bba4412527f36ea4359e58ac4d4
Sven Lüchow <sven.luechow@icloud.com>
2022-03-09 22:38:33 +0100

    Add Function to change store-color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import Play from './pages/Play';
 import History from './pages/History';
 import Navigation from './components/Navigation';
-
 import Player from './components/Player';
 
 export default function App() {
@@ -15,6 +14,7 @@ export default function App() {
         <Route path="/history" element={<History />} />
       </Routes>
       <Navigation />
+      <Player />
     </Wrapper>
   );
 }
